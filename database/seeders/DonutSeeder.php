d882c9e5e76a8036610ab035bcd0ce97ee45b0f7
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-19 20:59:34 +0200

    add: image paths to donutseeder

--- database/seeders/DonutSeeder.php
+++ database/seeders/DonutSeeder.php
@@ -10,21 +10,21 @@ class DonutSeeder extends Seeder
     public function run(): void
     {
         DB::table('donuts')->insert([
-            ['name' => 'Moonlit Meringue', 'seal_of_approval' => 4, 'price' => 8],
-            ['name' => 'Unicorn Rainbow', 'seal_of_approval' => 5, 'price' => 9.5],
-            ['name' => 'Starlight Sprinkle', 'seal_of_approval' => 3, 'price' => 7],
-            ['name' => 'Sunfire Glaze', 'seal_of_approval' => 5, 'price' => 8.5],
-            ['name' => 'Dragon’s Breath', 'seal_of_approval' => 4, 'price' => 9],
-            ['name' => 'Velvet Crème', 'seal_of_approval' => 2, 'price' => 6.5],
-            ['name' => 'Aurora Swirl', 'seal_of_approval' => 4, 'price' => 8.2],
-            ['name' => 'Midnight Cocoa', 'seal_of_approval' => 3, 'price' => 7.8],
-            ['name' => 'Royal Raspberry', 'seal_of_approval' => 5, 'price' => 9.2],
-            ['name' => 'Lemon Mist', 'seal_of_approval' => 3, 'price' => 7.3],
-            ['name' => 'Caramel Crown', 'seal_of_approval' => 4, 'price' => 8.7],
-            ['name' => 'Cherry Blossom', 'seal_of_approval' => 2, 'price' => 6.9],
-            ['name' => 'Mint Majesty', 'seal_of_approval' => 5, 'price' => 9],
-            ['name' => 'Berry Bliss', 'seal_of_approval' => 3, 'price' => 7.5],
-            ['name' => 'Golden Honeycomb', 'seal_of_approval' => 4, 'price' => 8.4],
+            ['name' => 'Moonlit Meringue', 'seal_of_approval' => 4, 'price' => 8, 'image' => 'storage/donuts/moonlit-meringue.jpg'],
+            ['name' => 'Unicorn Rainbow', 'seal_of_approval' => 5, 'price' => 9.5, 'image' => 'storage/donuts/unicorn-rainbow.jpg'],
+            ['name' => 'Starlight Sprinkle', 'seal_of_approval' => 3, 'price' => 7, 'image' => 'storage/donuts/starlight-sprinkle.jpg'],
+            ['name' => 'Sunfire Glaze', 'seal_of_approval' => 5, 'price' => 8.5, 'image' => 'storage/donuts/sunfire-glaze.jpg'],
+            ['name' => 'Dragon’s Breath', 'seal_of_approval' => 4, 'price' => 9, 'image' => 'storage/donuts/dragons-breath.jpg'],
+            ['name' => 'Velvet Crème', 'seal_of_approval' => 2, 'price' => 6.5, 'image' => 'storage/donuts/velvet-creme.jpg'],
+            ['name' => 'Aurora Swirl', 'seal_of_approval' => 4, 'price' => 8.2, 'image' => 'storage/donuts/aurora-swirl.jpg'],
+            ['name' => 'Midnight Cocoa', 'seal_of_approval' => 3, 'price' => 7.8, 'image' => 'storage/donuts/midnight-cocoa.jpg'],
+            ['name' => 'Royal Raspberry', 'seal_of_approval' => 5, 'price' => 9.2, 'image' => 'storage/donuts/royal-raspberry.jpg'],
+            ['name' => 'Lemon Mist', 'seal_of_approval' => 3, 'price' => 7.3, 'image' => 'storage/donuts/lemon-mist.jpg'],
+            ['name' => 'Caramel Crown', 'seal_of_approval' => 4, 'price' => 8.7, 'image' => 'storage/donuts/caramel-crown.jpg'],
+            ['name' => 'Cherry Blossom', 'seal_of_approval' => 2, 'price' => 6.9, 'image' => 'storage/donuts/cherry-blossom.jpg'],
+            ['name' => 'Mint Majesty', 'seal_of_approval' => 5, 'price' => 9, 'image' => 'storage/donuts/mint-majesty.jpg'],
+            ['name' => 'Berry Bliss', 'seal_of_approval' => 3, 'price' => 7.5, 'image' => 'storage/donuts/berry-bliss.jpg'],
+            ['name' => 'Golden Honeycomb', 'seal_of_approval' => 4, 'price' => 8.4, 'image' => 'storage/donuts/golden-honeycomb.jpg'],
         ]);
     }
 }
