707c7b40aeea3bd20a89c0f6b1068b6fc2007d50
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-19 19:53:36 +0200

    add: fetch donut list testcase

--- tests/Feature/DonutApiTest.php
+++ tests/Feature/DonutApiTest.php
@@ -1,7 +1,38 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+namespace Tests\Feature;
 
-    $response->assertStatus(200);
-});
+use App\Models\Donut;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DonutApiTest extends TestCase
+{
+    use RefreshDatabase; 
+
+    public function can_fetch_donut_list()
+    {
+        Donut::create([
+            'name' => 'Choco Glaze',
+            'price' => 7.5,
+            'seal_of_approval' => 4,
+        ]);
+
+        Donut::create([
+            'name' => 'Vanilla Sky',
+            'price' => 6.0,
+            'seal_of_approval' => 5,
+        ]);
+
+        $response = $this->getJson('/api/donuts');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => ['id', 'name', 'price', 'seal_of_approval']
+            ]
+        ]);
+
+        $response->assertJsonFragment([
+            'name' => 'Choco Glaze',
+        ]);
+    }
