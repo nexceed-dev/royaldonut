23ec37b64bbc4a44f76b5cc4c43c605ddff8c944
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-19 13:07:17 +0200

    add: donut api route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\DonutsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+route::apiResource('donuts', DonutsController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
