6e5e093be226d6c900364a40dddec748f41bd0fb
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-19 23:44:49 +0200

    update: add image to model

--- app/Models/Donut.php
+++ app/Models/Donut.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Donut extends Model
 {
     protected $table = 'donuts';
-    protected $fillable = ['name', 'price', 'seal_of_approval'];
+    protected $fillable = ['name', 'price', 'seal_of_approval', 'image'];
 }
