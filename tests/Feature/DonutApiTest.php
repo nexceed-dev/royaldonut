b323ffcb8487b21ca072284e45f671b0c235d1d6
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-19 20:06:20 +0200

    add: can sort by name and approval testcase

--- tests/Feature/DonutApiTest.php
+++ tests/Feature/DonutApiTest.php
@@ -4,12 +4,14 @@ namespace Tests\Feature;
 
 use App\Models\Donut;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class DonutApiTest extends TestCase
 {
     use RefreshDatabase; 
 
+    #[Test]
     public function can_fetch_donut_list()
     {
         Donut::create([
@@ -36,3 +38,30 @@ class DonutApiTest extends TestCase
             'name' => 'Choco Glaze',
         ]);
     }
+
+    #[Test]
+    public function can_sort_by_name_and_seal_of_approval()
+    {
+        Donut::create([
+            'name' => 'Apple Cinnamon',
+            'price' => 7.5,
+            'seal_of_approval' => 1,
+        ]);
+
+        Donut::create([
+            'name' => 'Zebra Stripes',
+            'price' => 6.0,
+            'seal_of_approval' => 5,
+        ]);
+
+        $response = $this->getJson('/api/donuts?sort=name&order=asc');
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.name', 'Apple Cinnamon');
+        $response->assertJsonPath('data.1.name', 'Zebra Stripes');
+
+        $response = $this->getJson('/api/donuts?sort=approval&order=desc');
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.seal_of_approval', 5);
+        $response->assertJsonPath('data.1.seal_of_approval', 1);
+    }
+}
