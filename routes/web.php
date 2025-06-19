04e7ceadcf2bcc3f26b5db63222ff1be827f8676
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-19 16:46:41 +0200

    add:  donut view route for index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Donut;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/donuts', function () {
+    return view('donuts.index');
 });
