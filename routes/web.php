9517498964d4e887963b552209e70ba687b448c3
d304219 <145758870+d304219@users.noreply.github.com>
2025-06-24 00:22:16 +0200

    update: changed route from /donuts to /

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/donuts', function () {
     return view('donuts.index');
 });
+
